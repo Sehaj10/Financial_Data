547898b0967ae5c57a25796a4aeadaa5cb9ff78a
Sehaj10 <113855383+Sehaj10@users.noreply.github.com>
2023-06-08 23:57:24 -0400

    Update app.py

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ def earnings(api_key, ticker_list):
             continue
         
         df = pd.DataFrame(data)
-        st.write(df.head())
+        st.dataframe(df.head())
         csv_file_path = 'file_{}_{}.csv'.format(interval, ticker.strip())
         df.to_csv(csv_file_path, index=False)
         st.write("CSV file saved:", csv_file_path)
@@ -45,7 +45,7 @@ def get_csv_download_button(file_path, ticker):
     with open(file_path, 'rb') as file:
         csv_data = file.read()
     base64_encoded = base64.b64encode(csv_data).decode()
-    download_button = f'<a href="data:file/csv;base64,{base64_encoded}" download="{ticker}.csv"><button>Download CSV</button></a>'
+    download_button = f'<a href="data:file/csv;base64,{base64_encoded}" download="{csv_file_path}.csv"><button>Download CSV</button></a>'
     return download_button
 
 earnings(key, ticker)
